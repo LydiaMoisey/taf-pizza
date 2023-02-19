070d0faf8064d6757c4eb5fdad231993d15d66b6
LydiaMoisey <mois.lydia@gmail.com>
2023-02-19 20:32:51 +0300

    refactor TerraPizzaPage and TerraPizzaTest classes, add a drink check

--- src/main/java/moiseenkolydia/TerraPizzaPage.java
+++ src/main/java/moiseenkolydia/TerraPizzaPage.java
@@ -5,8 +5,10 @@ public class TerraPizzaPage {
     public static final String COOKIES =  "//button[@class='close-icon']";
     public static final String MENU = "//li[@class='main-menu__item']/a[@href='https://terrapizza.by/menu']";
     public static final String PIZZA ="//div/a[@href='/catalog/picca']";
-  // public static final String PIZZA = "//li[@class='menu-navigation__item']/a[@href='/catalog/picca']";
-    public static final String ADD_TO_CART = "//button[@data-hrf='/cart/add-basket?id=364']";
+    public static final String DRINKS = "//li[@class='menu-navigation__item']/a[@href='/catalog/bar']";
+    public static final String ADD_TO_CART_PIZZA = "//button[@data-hrf='/cart/add-basket?id=364']";
+    public static final String ADD_TO_CART_DRINK = "//button[@data-hrf='/cart/add-basket?id=520']";
+    public static final String DRINK_NAME = ADD_TO_CART_DRINK + "/../../../preceding-sibling::*[@class='menu-card__title']";
     public static final String CART = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
     public static final String ITEM_IN_CART = "//div[@class='basket__products-item-name']";
 
--- src/test/java/moiseenkolydia/TerraPizzaTest.java
+++ src/test/java/moiseenkolydia/TerraPizzaTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -22,11 +23,12 @@ public class TerraPizzaTest {
         driver.manage().window().maximize();
         driver.get(TerraPizzaPage.URL);
         actions = new Actions(driver);
-        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+
     }
 
     @Test
     public void testCheckPizzaInCart() {
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
         WebElement CLOSE_COOKIE = driver.findElement(By.xpath(TerraPizzaPage.COOKIES));
         CLOSE_COOKIE.click();
         WebElement MENU_BTN = driver.findElement(By.xpath(TerraPizzaPage.MENU));
@@ -35,13 +37,38 @@ public class TerraPizzaTest {
         WebElement PIZZA_BTN = driver.findElement(By.xpath(TerraPizzaPage.PIZZA));
         PIZZA_BTN.click();
 
-        WebElement ADD_TO_CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.ADD_TO_CART));
+        WebElement ADD_TO_CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.ADD_TO_CART_PIZZA));
         ADD_TO_CART_BTN.click();
 
         WebElement CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.CART));
         CART_BTN.click();
         List<WebElement> PRODUCTS_IN_CART_LIST = driver.findElements(By.xpath(TerraPizzaPage.ITEM_IN_CART));
         Assertions.assertTrue(PRODUCTS_IN_CART_LIST.get(0).getText().contains("Пицца Маргарита"));
+    }
+    @Test
+    public void testCheckPizzaAndDrink() {
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        WebElement CLOSE_COOKIE = driver.findElement(By.xpath(TerraPizzaPage.COOKIES));
+        CLOSE_COOKIE.click();
+        WebElement MENU_BTN = driver.findElement(By.xpath(TerraPizzaPage.MENU));
+        MENU_BTN.click();
+
+        WebElement PIZZA_BTN = driver.findElement(By.xpath(TerraPizzaPage.PIZZA));
+        PIZZA_BTN.click();
+
+        WebElement ADD_PIZZA_TO_CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.ADD_TO_CART_PIZZA));
+        ADD_PIZZA_TO_CART_BTN.click();
+        WebElement DRINKS_BTN = driver.findElement(By.xpath(TerraPizzaPage.DRINKS));
+        DRINKS_BTN.click();
+        WebElement ADD_DRINKS_TO_CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.ADD_TO_CART_DRINK));
+        ADD_DRINKS_TO_CART_BTN.click();
+        WebElement DRINK_NAME = driver.findElement(By.xpath(TerraPizzaPage.DRINK_NAME));
+
+        WebElement CART_BTN = driver.findElement(By.xpath(TerraPizzaPage.CART));
+        CART_BTN.click();
+        List<WebElement> PRODUCTS_IN_CART_LIST = driver.findElements(By.xpath(TerraPizzaPage.ITEM_IN_CART));
+        Assertions.assertTrue(PRODUCTS_IN_CART_LIST.get(0).getText().contains("Пицца Маргарита"));
+        Assertions.assertTrue(PRODUCTS_IN_CART_LIST.get(1).getText().contains(DRINK_NAME.getText()));
     }
     @AfterEach
     public void tearDown() {
